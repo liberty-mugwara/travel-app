09c283c7184bcc8ba3da75e0d4d98b775f450bf5
Liberty Mugwara <libertymugwara@live.com>
2021-07-10 07:16:43 +0200

    feat: Add catching

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,10 +19,44 @@ app.use(
 );
 
 // API endpoint
-// const projectData = [];
+const projectData = {};
 
 // Router
-app.use("/add-trip/", tripRoutes);
+app.get("/add-trip/:location/date/:date", async (req, res) => {
+  try {
+    const location = req.params.location.toLowerCase();
+
+    if (projectData[location]) {
+      return res.json(projectData[location]);
+    }
+
+    const { lat, lon, cityName, countryName } = await getLocationData(location);
+
+    const [weather, imageUrl] = await Promise.all([
+      getWeather(lat, lon),
+      getCityImage(cityName, countryName),
+    ]);
+
+    projectData[location] = {
+      cityName,
+      countryName,
+      weather,
+      imageUrl,
+      departure: req.params.date,
+    };
+
+    res.json({
+      cityName,
+      countryName,
+      weather,
+      imageUrl,
+      departure: req.params.date,
+    });
+  } catch (err) {
+    res.status(404).json({ message: "not found" });
+    console.error(err);
+  }
+});
 
 // starting server
 const port = process.env.PORT || 8081;
@@ -34,3 +69,99 @@ app.listen(port, () =>
     )
   )
 );
+
+async function getLocationData(location) {
+  try {
+    const res = await axios({
+      method: "get",
+      url: "http://api.geonames.org/searchJSON?",
+      params: {
+        name: location,
+        name_equals: location,
+        style: "full",
+        maxRows: 3,
+        username: process.env.GEONAMES_USERNAME || "",
+      },
+    });
+    const target = res.data.geonames[0];
+    if (target) {
+      return {
+        lat: target.lat,
+        lon: target.lng,
+        cityName: target.name,
+        countryName: target.countryName,
+      };
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getWeather(latitude, longitude) {
+  try {
+    console.log(longitude, latitude);
+    const res = await axios({
+      method: "get",
+      url: "https://api.weatherbit.io/v2.0/current?",
+      params: {
+        key: process.env.WEATHERBIT_KEY || "",
+        lat: latitude,
+        lon: longitude,
+      },
+    });
+
+    const data = res.data.data[0];
+
+    return {
+      description: data.weather.description,
+      temp: data.temp,
+      app_temp: data.app_temp,
+    };
+  } catch (err) {
+    throw err.data;
+  }
+}
+
+async function getCityImage(city, country) {
+  try {
+    const res = await axios({
+      method: "get",
+      url: "https://pixabay.com/api/",
+      params: {
+        q: `${country}+${city}`,
+        key: process.env.PIXABAY_KEY || "",
+        image_type: "photo",
+        orientation: "horizontal",
+        category: "places",
+        per_page: 3,
+      },
+    });
+
+    const images = res.data.hits;
+    if (images.length) {
+      return images[0].webformatURL;
+    } else {
+      const response = await axios({
+        method: "get",
+        url: "https://pixabay.com/api/",
+        params: {
+          q: `${country}`,
+          key: process.env.PIXABAY_KEY || "",
+          image_type: "photo",
+          orientation: "horizontal",
+          category: "places",
+          per_page: 3,
+        },
+      });
+
+      const imgs = response.data.hits;
+      if (imgs.length) {
+        return imgs[0].webformatURL;
+      }
+    }
+
+    return "";
+  } catch (err) {
+    throw err;
+  }
+}
